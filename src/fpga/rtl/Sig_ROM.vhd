3e76aadece5b473234b3b033e4436d3f81b862a8
CramBL <marcbkonig@gmail.com>
2023-07-14 10:13:29 +0200

    clean up

--- src/fpga/rtl/Sig_ROM.vhd
+++ src/fpga/rtl/Sig_ROM.vhd
@@ -1,6 +1,6 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 entity Sig_ROM_VHDL is
     generic (
@@ -8,23 +8,23 @@ entity Sig_ROM_VHDL is
         dataWidth: integer := 16
     );
     port (
-        clk : in std_logic;
-        x   : in std_logic_vector(inWidth-1 downto 0);
-        out : out std_logic_vector(dataWidth-1 downto 0)
+        clk : in STD_LOGIC;
+        x   : in STD_LOGIC_VECTOR(inWidth-1 downto 0);
+        y_out : out STD_LOGIC_VECTOR(dataWidth-1 downto 0)
     );
 end entity Sig_ROM_VHDL;
 
 architecture behavioral of Sig_ROM_VHDL is
-    type mem_array is array (natural range <>) of std_logic_vector(dataWidth-1 downto 0);
+    type mem_array is array (natural range <>) of STD_LOGIC_VECTOR(dataWidth-1 downto 0);
     signal mem : mem_array(0 to 2**inWidth-1);
-    signal y   : std_logic_vector(inWidth-1 downto 0);
+    signal y_tmp   : STD_LOGIC_VECTOR(inWidth-1 downto 0);
 begin
 
     initialize: process
     begin
         file file_ptr: text;
         variable line_buf: line;
-        variable mem_val: std_logic_vector(dataWidth-1 downto 0);
+        variable mem_val: STD_LOGIC_VECTOR(dataWidth-1 downto 0);
         variable mem_index: integer := 0;
         variable file_open: boolean := FALSE;
         variable read_success: boolean := TRUE;
@@ -49,13 +49,13 @@ begin
     begin
         if rising_edge(clk) then
             if signed(x) >= 0 then
-                y <= std_logic_vector(unsigned(x) + 2**(inWidth-1));
+                y_tmp <= STD_LOGIC_VECTOR(unsigned(x) + 2**(inWidth-1));
             else
-                y <= std_logic_vector(unsigned(x) - 2**(inWidth-1));
+                y_tmp <= STD_LOGIC_VECTOR(unsigned(x) - 2**(inWidth-1));
             end if;
         end if;
     end process;
     
-    out <= mem(to_integer(unsigned(y)));
+    y_out <= mem(to_integer(unsigned(y_out)));
     
 end architecture behavioral;
