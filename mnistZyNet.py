759ccf782546d30b7ea496e8cbf73b2cebb7247b
CramBL <marcbkonig@gmail.com>
2023-07-23 17:02:25 +0200

    small code quality improvements

--- mnistZyNet.py
+++ mnistZyNet.py
@@ -1,4 +1,5 @@
 """ Script for making changes to the neuron model in zynet. """
+import sys
 import argparse
 from zynet import zynet
 from zynet import utils
@@ -20,11 +21,12 @@ def generate_mnist_zynet(
     model.add(zynet.layer("Dense", 10, neuron_type))
     model.add(zynet.layer("Dense", 10, "hardmax"))
 
-    neuron_file = "Sigmoid" # default
-    if neuron_type == "relu" or neuron_type == "relu-vhdl":
+    neuron_file = "Sigmoid"  # default
+    if neuron_type in ("relu", "relu-vhdl"):
         neuron_file = "ReLu"
 
-    weight_array = utils.genWeightArray("WeigntsAndBiases"+neuron_file+"New.txt")
+    weight_array = utils.genWeightArray(
+        "WeigntsAndBiases"+neuron_file+"New.txt")
     bias_array = utils.genBiasArray("WeigntsAndBiases"+neuron_file+"New.txt")
     model.compile(
         pretrained="Yes",
@@ -38,9 +40,9 @@ def generate_mnist_zynet(
 
     if args.make_project is True:
         print("Creating vivado project files as MyZyNetProject")
-        zynet.makeXilinxProject('MyZyNetProject','xc7z020clg484-1')
+        zynet.makeXilinxProject('MyZyNetProject', 'xc7z020clg484-1')
         zynet.makeIP('MyZyNetProject')
-        zynet.makeSystem('MyZyNetProject','myBlock2')
+        zynet.makeSystem('MyZyNetProject', 'myBlock2')
 
 
 if __name__ == "__main__":
@@ -49,35 +51,33 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--sigmoid-size", type=int)
     parser.add_argument("-m", "--make-project", action='store_true')
 
-
     args = parser.parse_args()
-
 
     print(f"Neuron type: {args.neuron}")
 
-    if (
-        args.neuron != "relu"
-        and args.neuron != "sigmoid"
-        and args.neuron != "relu-vhdl"
-        and args.neuron != "sigmoid-vhdl"
+    if args.neuron not in (
+        "relu", "sigmoid", "relu-vhdl", "sigmoid-vhdl"
     ):
         print("Neuron type must be relu, sigmoid, or relu-vhdl")
-        exit(1)
+        sys.exit(1)
+
     if args.neuron == "sigmoid-vhdl":
         print("Unfortunately Vivado lacks good support for VHDL I/O...")
-        print("Maybe it has better support now? You can try to fix this if you know what you're doing")
+        print("Maybe it has better support now?")
+        print("You can try to fix this if you know what you're doing")
         print("exiting...")
-        exit(1)
+        sys.exit(1)
 
-    sigmoid_size = args.sigmoid_size
-    if sigmoid_size is None:
-        sigmoid_size = 5 # default
-    if sigmoid_size > 10:
+    SIG_SIZE = args.sigmoid_size
+    if SIG_SIZE is None:
+        SIG_SIZE = 5  # default
+
+    if SIG_SIZE > 10:
         print("WARNING - Sigmoid size larger than 10 might fail to synthesize")
 
     generate_mnist_zynet(
-        data_width=16, # Default 16
-        sigmoid_size=sigmoid_size,
+        data_width=16,  # Default 16
+        sigmoid_size=SIG_SIZE,
         weight_int_size=4,
         input_int_size=1,
         neuron_type=args.neuron,
--- plots/plotting.py
+++ plots/plotting.py
@@ -1,5 +1,5 @@
+"""Basic plotting functionality"""
 import matplotlib.pyplot as plt
-import numpy as np
 
 SIGMOID_SIZE_BITS = [2, 3, 4, 5, 6, 7, 8, 9, 10]
 SIGMOID_ACCURACY = [8.5, 8.5, 11.6, 94.1, 94.5, 95.0, 95.1, 95.1, 95.1]
@@ -10,18 +10,20 @@ if __name__ == "__main__":
     x = SIGMOID_SIZE_BITS
     y = SIGMOID_ACCURACY
 
-    
+    fig, ax = plt.subplots(figsize=(12, 7))
 
-    fig, ax = plt.subplots(figsize=(12, 6.75))
-
-    ax.set(xlabel='Sigmoid size [bits]', ylabel='Accuracy [%]',
-        title='Sigmoid size vs. Accuracy (1000 predictions)')
+    ax.set_title('Sigmoid size vs. Accuracy (1000 predictions)', fontsize=20)
     ax.grid()
-    plt.plot(x, y, marker='o', linestyle='dashed')  
+    plt.plot(x, y, marker='o', linestyle='dashed')
     # Annotate the values at each step
-    for i in range(len(x)):
-        ax.annotate(f'{y[i]:.1f}%', (x[i], y[i]), textcoords="offset points", xytext=(0, 5), ha='center')
-    
+    for i, val in enumerate(x):
+        ax.annotate(f'{y[i]:.1f}%', (val, y[i]),
+                    textcoords="offset points", xytext=(0, 5), ha='center')
+
+    # Increase the font size of the axis labels
+    ax.set_xlabel("Sigmoid size [bits]", fontsize=16)
+    ax.set_ylabel("Accuracy [%]", fontsize=16)
+
     # Remove white space around the figure
     plt.tight_layout()
 
